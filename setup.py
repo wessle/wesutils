6dbd026acf90629cfd761e6f266ff9be9bdecb67
Wes <suttlewes@gmail.com>
2020-03-10 17:06:46 -0400

    removed malfunctioning SACAgent from master, left it on dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wesutils",
-    version="0.0.5",
+    version="0.0.7",
     author="Wesley Suttle",
     description="my most commonly used utilities",
     long_description=long_description,
